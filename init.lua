3fb26c8ca11f06adfc9a5b0d6a84086e4a1b3f0c
littleblack111 <jerrylam110209@gmail.com>
2023-06-14 20:59:59 +0800

    Revert "more plugin included and features"
    This reverts commit c74434e4df735d2c057600031adce0f3b526aa11.
    
    	修改：     init.lua
    	修改：     lua/custom/packer.lua
    	修改：     plugin/packer_compiled.lua
    	修改：     lua/custom/init.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,5 @@
 require("custom")
 
---local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
---vim.api.nvim_create_autocmd('BufWritePost packer.lua', {
---    command = 'source | PackerCompile',
---    group = packer_group,
---})
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -31,7 +25,7 @@ Plug 'https://github.com/tpope/vim-commentary'
 Plug 'https://github.com/lifepillar/pgsql.vim'
 Plug 'https://github.com/ap/vim-css-color'
 Plug 'https://github.com/rafi/awesome-vim-colorschemes'
---Plug 'https://github.com/neoclide/coc.nvim'
+Plug 'https://github.com/neoclide/coc.nvim'
 Plug 'https://github.com/ryanoasis/vim-devicons'
 Plug 'https://github.com/tc50cal/vim-terminal'
 Plug 'https://github.com/preservim/tagbar'
@@ -39,5 +33,3 @@ Plug 'https://github.com/terryma/vim-multiple-cursors'
 
 
 vim.call('plug#end')
-
-
