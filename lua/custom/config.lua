dd04fbbd35fcd17099d588aadd224da36b000a0d
littleblack111 <jerrylam110209@gmail.com>
2023-06-15 18:22:20 +0800

    added ccls, removed/migrated: coc -> lsp, nerdtree -> nvimtree, airline -> lualine

--- lua/custom/config.lua
+++ lua/custom/config.lua
@@ -7,7 +7,7 @@ vim.api.colorscheme = "tokyonight-storm"
 vim.opt.relativenumber = true
 vim.api.nvim_set_hl(0, 'LineNr', { fg = "white"})
 vim.api.nvim_command("set runtimepath+=~/.vim_runtime")
---vim.api.nvim_command("source ~/.vim_runtime/vimrcs/basic.vim")
+vim.api.nvim_command("source ~/.vim_runtime/vimrcs/basic.vim")
 vim.api.nvim_command("source ~/.vim_runtime/vimrcs/filetypes.vim")
 --vim.api.nvim_command("source ~/.vim_runtime/vimrcs/plugins_config.vim")
 vim.api.nvim_command("source ~/.vim_runtime/vimrcs/extended.vim")
