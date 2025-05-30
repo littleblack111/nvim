b823b1bf54da7ef8f7f0b9d4aa138f5a1d8fb67c
littleblack111 <littleblack11111@gmail.com>
2025-05-30 21:06:53 +0800

    conform: custom clang-formatter for default style

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -4,9 +4,25 @@ local options = {
     lsp_fallback = true,
   },
 
+  format_on_save_timeout = 500,
+  formatters_by_ft = {
+    c = { "clang_format" },
+    cpp = { "clang_format" },
+  },
+
   formatters = {
     shfmt = {
       prepend_args = { "-i", "2", "-ci" },
+    },
+    clang_format = {
+      prepend_args = function()
+        local root = vim.fn.getcwd()
+        if vim.fn.filereadable(root .. "/.clang-format") == 1 then
+          return { "--style=file" }
+        else
+          return { "--style", "{IndentWidth: 4, UseTab: Always, TabWidth: 4}" }
+        end
+      end,
     },
   },
 
